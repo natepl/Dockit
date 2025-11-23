5fe2e1e2ccc80b1d45ca4e19e05dc6524f2eee95
natepl <68605773+natepl@users.noreply.github.com>
2025-11-22 22:17:16 -0500

    Initial Commit

---
+++ Dockit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dockit
+//
+//  Created by Nathaniel Parry Luff on 11/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dockit/DockitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DockitApp.swift
+//  Dockit
+//
+//  Created by Nathaniel Parry Luff on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct DockitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
